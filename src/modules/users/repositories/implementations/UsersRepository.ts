62dd719d179be1e4686147e2a7a644deb78ab593
Renan.Correa <renan.rcp16@gmail.com>
2022-11-24 11:51:43 -0300

    iniciando desafio

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,31 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+	const user = await this.repository.findOneOrFail({where: {id: user_id}})
+    return user
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC'); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(`SELECT * FROM users WHERE first_name = '${first_name}' AND last_name = '${last_name}'`); // Complete usando raw query
+  }
+}
